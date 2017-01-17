2a74160e73d47117679b9276e4def9dc1614bd81
Nicole Sanders <nsanders9022@gmail.com>
2017-01-16 16:32:42 -0800

    fix card assignment

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
 
     endArray.forEach(function(item){
       $("#result").append("<li>" + item + "</li>");
-    })
+    });
 
   });
 
@@ -56,46 +56,42 @@ $(document).ready(function(){
 
 //---------------------------------------------------------------------
 
-// //card deck
-// $(document).ready(function(){
-//   $("p.deck").text(cardDeck());
-// });
-//
-// var cardDeck = function(){
-//   var numbers = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
-//   var suits = ["clubs", "diamonds", "spades", "hearts"];
-//
-//   suits.forEach(function(suit){
-//     numbers.forEach(function(number){
-//       // $("ul.deck").append("<li>" + number + " of " + suit + "</li>");
-//       return number + " of " + suit;
-//     });
-//   });
-// };
+//card deck
+$(document).ready(function(){
+
+  var numbers = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
+  var suits = ["clubs", "diamonds", "spades", "hearts"];
+
+  suits.forEach(function(suit){
+    numbers.forEach(function(number){
+      $("ul.deck").append("<li>" + number + " of " + suit + "</li>");
+    });
+  });
+});
 
 //---------------------------------------------------------------------
 
-//word play
-// $(document).ready(function(){
-//   $("form#word-play").submit(function(event){
-//     event.preventDefault();
-//
-//     var aSentence = $("#sentence").val()
-//
-//     $("#result").text(wordPlay(aSentence))
-//   })
-// });
-//
-// var wordPlay = function(sentence){
-//   var array = sentence.split(" ");
-//   var newArray = []
-//
-//   array.forEach(function(word){
-//     if (word.length >= 3){
-//       newArray.push(word)
-//     }
-//   })
-//
-//   newArray.reverse();
-//   return newArray.join();
-// };
+// word play
+$(document).ready(function(){
+  $("form#word-play").submit(function(event){
+    event.preventDefault();
+
+    var aSentence = $("#sentence").val()
+
+    $("#result").text(wordPlay(aSentence))
+  })
+});
+
+var wordPlay = function(sentence){
+  var array = sentence.split(" ");
+  var newArray = []
+
+  array.forEach(function(word){
+    if (word.length >= 3){
+      newArray.push(word)
+    }
+  })
+
+  newArray.reverse();
+  return newArray.join();
+};
