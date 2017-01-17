e0880a48a136d058d99cbe0d676bcaf4e67bcd63
Nicole Sanders <nsanders9022@gmail.com>
2017-01-16 16:15:09 -0800

    fix grocery problem

--- js/scripts.js
+++ js/scripts.js
@@ -26,62 +26,65 @@ $(document).ready(function(){
   $("form#groceries").submit(function(event){
     event.preventDefault();
 
-    newList.forEach(function(){
+    var list = ["one", "two", "three", "four", "five"];
+    var newList = []
+
+    list.forEach(function(item){
+      var userInput = $("input#" + item).val();
+      userInput = userInput.toUpperCase();
+      newList.push(userInput);
+    });
+
+    newList.sort();
+
+    newList.forEach(function(item){
+      $("#shopping-list").append("<li>" + item + "</li>")
     });
   });
 });
-
-var groceryList = function (grocery1, grocery2, grocery3, grocery4, grocery5){
-  var list = [one, two, three, four, five];
-
-  list.forEach(function(item){
-    var userInput = $("input#" + item).val();
-  });
-};
-
 
 //---------------------------------------------------------------------
 
-//card deck
-$(document).ready(function(){
-  $("ul.deck").append("<li>" + cardDeck() + "</li>");
-});
-
-var cardDeck = function(){
-  var numbers = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
-  var suits = ["clubs", "diamonds", "spades", "hearts"];
-
-  suits.forEach(function(suit){
-    numbers.forEach(function(number){
-      // $("ul.deck").append("<li>" + number + " of " + suit + "</li>");
-      return number + " of " + suit;
-    });
-  });
-};
+// //card deck
+// $(document).ready(function(){
+//   $("p.deck").text(cardDeck());
+// });
+//
+// var cardDeck = function(){
+//   var numbers = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
+//   var suits = ["clubs", "diamonds", "spades", "hearts"];
+//
+//   suits.forEach(function(suit){
+//     numbers.forEach(function(number){
+//       // $("ul.deck").append("<li>" + number + " of " + suit + "</li>");
+//       return number + " of " + suit;
+//     });
+//   });
+// };
 
 //---------------------------------------------------------------------
 
 //word play
-$(document).ready(function(){
-  $("form#word-play").submit(function(event){
-    event.preventDefault();
-
-    var aSentence = $("#sentence").val()
-
-    $("#result").text(wordPlay(aSentence))
-  })
-});
-
-var wordPlay = function(sentence){
-  var array = sentence.split(" ");
-  var newArray = []
-
-  array.forEach(function(word){
-    if (word.length >= 3){
-      newArray.push(word)
-    }
-  })
-
-  newArray.reverse();
-  return newArray.join();
-};
+// $(document).ready(function(){
+//   $("form#word-play").submit(function(event){
+//     event.preventDefault();
+//
+//     var aSentence = $("#sentence").val()
+//
+//     $("#result").text(wordPlay(aSentence))
+//   })
+// });
+//
+// var wordPlay = function(sentence){
+//   var array = sentence.split(" ");
+//   var newArray = []
+//
+//   array.forEach(function(word){
+//     if (word.length >= 3){
+//       newArray.push(word)
+//     }
+//   })
+//
+//   newArray.reverse();
+//   return newArray.join();
+// };
